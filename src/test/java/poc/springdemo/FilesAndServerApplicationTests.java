05108cfbe61c0c577f85af755fab50280cea992a
ShiveTechie <shivatechnocrat@gmail.com>
2021-08-28 20:45:42 +0530

    Tests removed

--- src/test/java/poc/springdemo/FilesAndServerApplicationTests.java
+++ src/test/java/poc/springdemo/FilesAndServerApplicationTests.java
@@ -1,13 +1,13 @@
-package poc.springdemo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FilesAndServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package poc.springdemo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FilesAndServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
